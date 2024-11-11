7febf48a0f8c91ff22c44356e31897777f2aa165
ABarsen <kiplagatemmanuel21@gmail.com>
2024-11-11 10:29:54 +0300

    learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,8 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 
-
-// export default function Form (){
-//   const [answer, setAnswer] = useState('');
-//   const [error, setError ] = useState (null);
-//   const [status, setStatus] = useState ('typing');
-
-//   if (status === "success") {
-//     return <h1>thats is right</h1>
-//   }
-
-//   async function handleSubmit (e) {
-//     e.preventDefault ();
-//     setStatus ('submitting');
-//     try {
-//       await submitForm (answer);
-//       setStatus ('success');
-//     }
-//     catch (err) {
-//       setStatus ('typing');
-//       setError (err);
-//     }
-//   }
-//   function handleTextareaChange (e){
-//     setAnswer (e.target.value);
-
-//   }
-//   return(
-    
-//     <form>
-//     <h2>City quizez</h2>
-//     <p>in which city is there a billboard that turns air into drinkable water</p>
-
-//     <form onSubmit ={handleSubmit} >
-//         <textarea
-//         value = {answer}
-//         onChange={handleTextareaChange}
-//         disabled = {status =="submitting"}
-//         />
-//     <br />
-
-//     <button disabled ={
-//       answer.length === 0 ||
-//       status ==="submitting"}>
-//       submit
-//       </button>
-
-//       {error !==null && 
-//       <p className="Error">
-//         {error.message}
-//       </p>
-//       }
-//     </form>
-//     </form>
-//   );
-// }
-// function submitForm (answer){
-//   //pretend it is hitting a network
-
-//   return new Promise ((resolve ,reject) => {
-//     setTimeout(()=> {
-//       let shouldError = answer.toLowerCase() !== "lima , london"
-//       if (shouldError){
-//         reject(new Error ("good guess but a wrong answer. try again"));
-
-//     }
-//       else {
-//         resolve();
-//       } 
-//     },1500)
-//   });
-// }
-import {"useState" }from react
 export default function Accordion (){
-  const [activeIndex, setActiveIndex] =useState(0);
+  const [activeIndex, setActiveIndex] = useState (0);
   return(
     <>
     <h2>Nairobi , kenya</h2>
@@ -95,14 +23,14 @@ export default function Accordion (){
   );
 }
 
-function panel ({
+function Panel ({
   title,
   children,
   isActive,
   onShow
 }) {
   return (
-    <section className="panel">
+    <section className="Panel">
       <h3>{title}</h3>
       {isActive? (
         <p>{children}</p>
